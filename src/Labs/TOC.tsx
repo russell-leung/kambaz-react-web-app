3b259ecf0e90015f83088bb1c3837ec3c93f42c0
russell-leung <russell-leung@comcast.net>
2025-01-15 21:33:35 -0500

    Add missing stuff

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -2,12 +2,16 @@ import { Link } from 'react-router-dom';
 
 export default function TOC() {
   return (
+    <>
+    <h2>Table of Contents</h2>
     <ul>
       <li><Link to="/Labs">Labs</Link></li>
       <li><Link to="/Labs/Lab1">Lab 1</Link></li>
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
       <li><Link to="/Kambaz">Kambaz</Link></li>
+      <li><a id="wd-github" href="https://github.com/russell-leung/kambaz-react-web-app">GitHub</a></li>
     </ul>
+    </>
   );
 }
